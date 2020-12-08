dc9688c8cac0d67007437a8681bc84b908e1146a
aldoreghan <aldoreghan2217@gmail.com>
2020-12-08 17:44:14 +0700

    add drawer

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -16,91 +16,124 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: [
-          Stack(
-            children: [
-              Container(
-                color: Colors.indigo,
-                height: 100,
-                child: Padding(
-                  padding: const EdgeInsets.only(top: 40, left: 10, right: 10),
-                  child: Container(
-                    height: 40,
-                    margin: EdgeInsets.only(bottom: 15),
-                    decoration: BoxDecoration(
+  GlobalKey<ScaffoldState> _key = GlobalKey();
+
+  buildDrawer() {
+    return Drawer();
+  }
+
+  Widget setAppBar(GlobalKey<ScaffoldState> globalKey) {
+    return Stack(
+      children: [
+        Container(
+          color: Colors.indigo,
+          height: 100,
+          child: Padding(
+            padding: const EdgeInsets.only(top: 40, left: 10, right: 10),
+            child: Row(
+              children: [
+                Container(
+                    margin: EdgeInsets.only(bottom: 15, right: 10),
+                    child: GestureDetector(
+                      onTap: () {
+                        globalKey.currentState.openDrawer();
+                      },
+                      child: Icon(
+                        Icons.list,
+                        size: 35,
                         color: Colors.white,
-                        borderRadius: BorderRadius.circular(10)),
-                    child: TextFormField(
-                      style: TextStyle(fontSize: 16, color: Colors.black),
-                      decoration: InputDecoration(
-                        border: InputBorder.none,
-                        hintText: 'Search Idea...',
-                        hintStyle: TextStyle(fontSize: 16, color: Colors.grey),
-                        prefixIcon: Icon(
-                          Icons.search,
-                          color: Colors.indigo,
-                        ),
+                      ),
+                    )),
+                Container(
+                  height: 40,
+                  width: MediaQuery.of(context).size.width / 1.2,
+                  margin: EdgeInsets.only(bottom: 15),
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(10)),
+                  child: TextFormField(
+                    style: TextStyle(fontSize: 16, color: Colors.black),
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      hintText: 'Search Idea...',
+                      hintStyle: TextStyle(fontSize: 16, color: Colors.grey),
+                      prefixIcon: Icon(
+                        Icons.search,
+                        color: Colors.indigo,
                       ),
                     ),
                   ),
-                ),
-              ),
-            ],
-          ),
-          Container(
-            height: 50,
-            padding: EdgeInsets.all(2),
-            child: ListView(
-              scrollDirection: Axis.horizontal,
-              children: [
-                Card(
-                  color: Colors.indigo,
-                  child: Container(
-                      width: 100,
-                      child: Center(
-                          child: Text(
-                        "Teknologi",
-                        style: TextStyle(color: Colors.white),
-                      ))),
-                ),
-                Card(
-                  color: Colors.indigo,
-                  child: Container(
-                      width: 100,
-                      child: Center(
-                          child: Text(
-                        "Masakan",
-                        style: TextStyle(color: Colors.white),
-                      ))),
-                ),
-                Card(
-                  color: Colors.indigo,
-                  child: Container(
-                      width: 100,
-                      child: Center(
-                          child: Text(
-                        "Kesehatan",
-                        style: TextStyle(color: Colors.white),
-                      ))),
-                ),
-                Card(
-                  color: Colors.indigo,
-                  child: Container(
-                      width: 100,
-                      child: Center(
-                          child: Text(
-                        "Pertambangan",
-                        textAlign: TextAlign.center,
-                        style: TextStyle(color: Colors.white),
-                      ))),
                 )
               ],
             ),
           ),
+        ),
+      ],
+    );
+  }
+
+  buildListKategori() {
+    return Container(
+      height: 50,
+      padding: EdgeInsets.all(2),
+      child: ListView(
+        scrollDirection: Axis.horizontal,
+        children: [
+          Card(
+            color: Colors.indigo,
+            child: Container(
+                width: 100,
+                child: Center(
+                    child: Text(
+                  "Teknologi",
+                  style: TextStyle(color: Colors.white),
+                ))),
+          ),
+          Card(
+            color: Colors.indigo,
+            child: Container(
+                width: 100,
+                child: Center(
+                    child: Text(
+                  "Masakan",
+                  style: TextStyle(color: Colors.white),
+                ))),
+          ),
+          Card(
+            color: Colors.indigo,
+            child: Container(
+                width: 100,
+                child: Center(
+                    child: Text(
+                  "Kesehatan",
+                  style: TextStyle(color: Colors.white),
+                ))),
+          ),
+          Card(
+            color: Colors.indigo,
+            child: Container(
+                width: 100,
+                child: Center(
+                    child: Text(
+                  "Pertambangan",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(color: Colors.white),
+                ))),
+          )
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      key: _key,
+      drawer: buildDrawer(),
+      body: Column(
+        children: [
+          setAppBar(_key),
+          buildListKategori(),
           Divider(
             color: Colors.black,
           )
