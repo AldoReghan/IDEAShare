498396a8acae2b48cf78dcaf01261ebcaf17c653
aldoreghan <aldoreghan2217@gmail.com>
2021-02-05 03:59:45 +0700

    detail idea pages update

--- lib/component/cardItem.dart
+++ lib/component/cardItem.dart
@@ -1,91 +1,93 @@
 import 'package:flutter/material.dart';
 import 'package:ideashare/component/buttonCard.dart';
 
-cardItem(BuildContext context,int iditem,String judul,String kategori,String image){
+cardItem(BuildContext context, int iditem, String judul, String kategori,
+    String image) {
   double height = MediaQuery.of(context).size.height;
   return Container(
-      height: height - kToolbarHeight * 2.9,
-      child: ListView(
-        padding: EdgeInsets.only(bottom: 4),
-        children: [
-          Container(
-            height: 150,
-            child: Card(
-              elevation: 4,
-              child: Container(
-                child: Row(
-                  children: <Widget>[
-                    Container(
-                      height: 150,
-                      width: 110,
-                      decoration: BoxDecoration(
-                        color: Colors.indigo,
-                        borderRadius: BorderRadius.only(
-                            topLeft: Radius.circular(5),
-                            bottomLeft: Radius.circular(5)),
-                        boxShadow: [
-                          BoxShadow(
-                            blurRadius: 2,
-                            offset: Offset(1, 3),
-                          )
-                        ],
-                      ),
+    height: height - kToolbarHeight * 2.9,
+    child: ListView(
+      padding: EdgeInsets.only(bottom: 4),
+      children: [
+        Container(
+          height: 150,
+          child: Card(
+            elevation: 4,
+            child: Container(
+              child: Row(
+                children: <Widget>[
+                  Container(
+                    height: 150,
+                    width: 110,
+                    decoration: BoxDecoration(
+                      color: Colors.indigo,
+                      borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(5),
+                          bottomLeft: Radius.circular(5)),
+                      boxShadow: [
+                        BoxShadow(
+                          blurRadius: 2,
+                          offset: Offset(1, 3),
+                        )
+                      ],
                     ),
-                    SizedBox(
-                      width: 5.0,
-                    ),
-                    Flexible(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          MergeSemantics(
-                            child: Row(
-                              children: <Widget>[
-                                Flexible(
-                                  child: Padding(
-                                    padding: const EdgeInsets.all(8.0),
-                                    child: Text(
-                                      judul,
-                                      overflow: TextOverflow.clip,
-                                      softWrap: true,
-                                      style: TextStyle(
-                                          fontSize: 14,
-                                          fontWeight: FontWeight.w600,
-                                          color:
-                                              Theme.of(context).primaryColor),
-                                    ),
+                  ),
+                  SizedBox(
+                    width: 5.0,
+                  ),
+                  Flexible(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        MergeSemantics(
+                          child: Row(
+                            children: <Widget>[
+                              Flexible(
+                                child: Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Text(
+                                    judul,
+                                    overflow: TextOverflow.clip,
+                                    softWrap: true,
+                                    style: TextStyle(
+                                        fontSize: 14,
+                                        fontWeight: FontWeight.w600,
+                                        color: Theme.of(context).primaryColor),
                                   ),
-                                )
-                              ],
-                            ),
-                          ),
-                          SizedBox(height: 5),
-                          Row(
-                            children: [
-                              Padding(
-                                padding:
-                                    const EdgeInsets.only(left: 8.0, bottom: 8),
-                                child: Text(
-                                  kategori,
-                                  style: TextStyle(
-                                      fontSize: 13,
-                                      fontWeight: FontWeight.w700,
-                                      color: Colors.black),
                                 ),
-                              ),
+                              )
                             ],
                           ),
-                          SizedBox(height: 15,),
-                          buttonCard(context,'Discuss','/detailIdea')
-                        ],
-                      ),
-                    )
-                  ],
-                ),
+                        ),
+                        SizedBox(height: 5),
+                        Row(
+                          children: [
+                            Padding(
+                              padding:
+                                  const EdgeInsets.only(left: 8.0, bottom: 8),
+                              child: Text(
+                                kategori,
+                                style: TextStyle(
+                                    fontSize: 13,
+                                    fontWeight: FontWeight.w700,
+                                    color: Colors.black),
+                              ),
+                            ),
+                          ],
+                        ),
+                        SizedBox(
+                          height: 15,
+                        ),
+                        buttonCard(context, 'Discuss', '/detailIdea')
+                      ],
+                    ),
+                  )
+                ],
               ),
             ),
           ),
-        ],
-      ),
-    );
+        ),
+      ],
+    ),
+  );
 }
--- lib/pages/detail_idea.dart
+++ lib/pages/detail_idea.dart
@@ -90,10 +90,67 @@ class _DetailIdeaPageState extends State<DetailIdeaPage> {
           child: Column(children: [
             setAppBar(_key),
             Container(
-              height: height/4,
+              height: height / 4,
               color: Colors.red,
             ),
-            SizedBox(height: 5,)
+            SizedBox(
+              height: 5,
+            ),
+            Padding(
+              padding: EdgeInsets.all(8),
+              child: Container(
+                width: MediaQuery.of(context).size.width,
+                child: Material(
+                  elevation: 10,
+                  borderRadius: BorderRadius.circular(10),
+                  child: Padding(
+                    padding: EdgeInsets.all(8),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('Data of Aldo Reghan',style: TextStyle(
+                              fontSize: 16, fontWeight: FontWeight.bold),),
+                        SizedBox(
+                          height: 5,
+                        ),
+                        Text('Skill : Advance'),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 5,
+            ),
+            Padding(
+              padding: EdgeInsets.all(8),
+              child: Container(
+                width: MediaQuery.of(context).size.width,
+                child: Material(
+                  elevation: 10,
+                  borderRadius: BorderRadius.circular(10),
+                  child: Padding(
+                    padding: EdgeInsets.all(8),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Description About Idea',
+                          style: TextStyle(
+                              fontSize: 16, fontWeight: FontWeight.bold),
+                        ),
+                        SizedBox(
+                          height: 5,
+                        ),
+                        Text(
+                            'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum'),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            )
           ]),
         ),
         bottomNavigationBar: buttonBottom());
